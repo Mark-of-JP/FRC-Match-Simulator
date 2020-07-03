68dd7ae2d8e045a4f2de076d07be82f866fa86bd
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-07-03 17:43:10 -0400

    Overhaul with elo

--- data_cleaner.py
+++ data_cleaner.py
@@ -21,7 +21,7 @@ awards_df = pd.read_csv('raw_frc_awards.csv')
 #Removal of rows and columns
 
 #Remove unwanted columns
-match_df = match_df.drop(columns=['Unnamed: 0', 'actual_time', 'predicted_time', 'set_number', 'match_number', 'comp_level'])
+match_df = match_df.drop(columns=['Unnamed: 0', 'actual_time', 'predicted_time'])
 
 #Remove rows that have matches that don't contain 3 teams per alliance
 for i in range(3):
@@ -46,7 +46,7 @@ for i in range(3):
 
 #Change unix time to datetime
 match_df['time'] = pd.to_datetime(match_df['time'], unit='s')
-match_df.sort_values(by='time')
+match_df.sort_values(by='time', inplace=True)
 
 #Reset the index
 match_df = match_df.reset_index(drop=True)
@@ -83,6 +83,8 @@ def initialize_team(team_key):
     team_data[team_key]['games_played_season'] = 0
     team_data[team_key]['games_won_season'] = 0
     team_data[team_key]['points_season'] = 0
+    
+    team_data[team_key]['elo'] = 1500
     
     team_data[team_key]['rookie_year'] = team_df[team_df['key'] == team_key]['rookie_year'].item()
     
@@ -125,6 +127,13 @@ for year in range(earliest_award_year, earliest_match_year):
         else:
             team_data[team_key]['other_awards'] += 1
             
+#Stores number of matches for giving awards from events
+print("Recording number of matches per event")
+num_of_matches_per_event = {}
+event_groupby = match_df.groupby('event_key')
+for event_key, group in event_groupby:
+    num_of_matches_per_event[event_key] = len(match_df[match_df.event_key == event_key])
+            
 #Adding team data to matches
 print('Adding team data to matches...')
 match_df['red_avg_match_awards'] = 0
@@ -142,12 +151,15 @@ match_df['blue_avg_games_played'] = 0
 match_df['red_avg_age'] = 0
 match_df['blue_avg_age'] = 0
 
-match_df['red_points_ratio'] = 0
+match_df['red_points_ratio_season'] = 0
 match_df['red_avg_winrate_season'] = 0
 match_df['red_avg_games_played_season'] = 0
 match_df['blue_avg_winrate_season'] = 0
 match_df['blue_avg_games_played_season'] = 0
 
+match_df['red_elo'] = 0
+match_df['blue_elo'] = 0
+match_df['elo_prediction'] = 0
 
 def get_avg_awards(alliance):
     total_match_awards = 0.0
@@ -159,17 +171,17 @@ def get_avg_awards(alliance):
         
     return total_match_awards / len(alliance), total_other_awards / len(alliance)
 
-def get_avg_games(alliance):
+def get_avg_games(alliance, boundary=''):
     total_games_played = 0
-    total_winrate = 0
+    total_wins = 0
     
     for team_key in alliance:
-        total_games_played += team_data[team_key]['games_played']
+        total_games_played += team_data[team_key]['games_played' + boundary]
+        total_wins += team_data[team_key]['games_won' + boundary]
         
-        if team_data[team_key]['games_played'] != 0:
-            total_winrate += (team_data[team_key]['games_won'] / team_data[team_key]['games_played'])
+    winrate = 0.5 if total_games_played == 0 else total_wins / total_games_played
         
-    return total_games_played / len(alliance), total_winrate / len(alliance)
+    return total_games_played / len(alliance), winrate
 
 def get_avg_age(alliance, current_year):
     total_age = 0
@@ -179,49 +191,63 @@ def get_avg_age(alliance, current_year):
         
     return total_age / len(alliance)
 
-def get_avg_games_season(alliance):
-    total_games_played = 0
-    total_winrate = 0
-    
-    for team_key in alliance:
-        total_games_played += team_data[team_key]['games_played_season']
-        
-        if team_data[team_key]['games_played_season'] != 0:
-            total_winrate += (team_data[team_key]['games_won_season'] / team_data[team_key]['games_played_season'])
-        
-    return total_games_played / len(alliance), total_winrate / len(alliance)
-
-def get_red_points_ratio(red_alliance, blue_alliance):
+def get_red_points_ratio(red_alliance, blue_alliance, boundary='_season'):
     total_red_avgs = 0
     total_blue_avgs = 0
     
     for team_key in red_alliance:
-        if team_data[team_key]['games_played_season'] != 0:
-            total_red_avgs += (team_data[team_key]['points_season'] / team_data[team_key]['games_played_season'])
+        if team_data[team_key]['games_played' + boundary] == 0:
+            return 1
+        
+        total_red_avgs += (team_data[team_key]['points' + boundary] / team_data[team_key]['games_played' + boundary])
             
     for team_key in blue_alliance:
-        if team_data[team_key]['games_played_season'] != 0:
-            total_blue_avgs += (team_data[team_key]['points_season'] / team_data[team_key]['games_played_season'])
+        if team_data[team_key]['games_played' + boundary] == 0:
+            return 1
+        total_blue_avgs += (team_data[team_key]['points' + boundary] / team_data[team_key]['games_played' + boundary])
     
     if total_blue_avgs == 0 or total_red_avgs == 0:
         return 1
     
     return total_red_avgs / total_blue_avgs
 
+def get_elo_average(alliance):
+    total_elo = 0
+    
+    for team_key in alliance:
+        total_elo += team_data[team_key]['elo']
+        
+    return total_elo / len(alliance)
+
+def get_elo_prediction(red_alliance_elo, blue_alliance_elo):
+    elo_delta = blue_alliance_elo - red_alliance_elo
+    
+    return 1 / (1 + (10**(elo_delta / 400)))
+
+def change_elo(alliance, prediction, result):
+    for team_key in alliance:
+        team_data[team_key]['elo'] += 10 * (result - prediction)
+
 def reset_season():
     
     for team_key in team_data:
+        team_data[team_key]['games_played'] += team_data[team_key]['games_played_season']
+        team_data[team_key]['games_won'] += team_data[team_key]['games_won_season']
+        
         team_data[team_key]['games_played_season'] = 0
         team_data[team_key]['games_won_season'] = 0
         team_data[team_key]['points_season'] = 0
-        
 
-curr_event = None
+curr_year = None
 len_match = len(match_df)
 match_df = match_df.reset_index(drop=True)
 for index, row in match_df.iterrows():
     
     print('Engineering features for match ' + str(index) + "/" + str(len_match))
+    
+    if row['year'] != curr_year:
+        curr_year = row['year']
+        reset_season()
         
     #Get the red and blue alliance
     red_alliance = [row['red_0'], row['red_1'], row['red_2']]
@@ -233,12 +259,6 @@ for index, row in match_df.iterrows():
             initialize_team(red_alliance[i])
         if blue_alliance[i] not in team_data:
             initialize_team(blue_alliance[i])
-            
-    #Add new event awards if previous event has passed
-    if row['event_key'] != curr_event:
-        add_event_awards(curr_event)
-        curr_event = row['event_key']
-        reset_season()
         
     row['red_avg_match_awards'], row['red_avg_other_awards'] = get_avg_awards(red_alliance)
     row['blue_avg_match_awards'], row['blue_avg_other_awards'] = get_avg_awards(blue_alliance)
@@ -246,32 +266,49 @@ for index, row in match_df.iterrows():
     row['red_avg_games_played'], row['red_avg_winrate'] = get_avg_games(red_alliance)
     row['blue_avg_games_played'], row['blue_avg_winrate'] = get_avg_games(blue_alliance)
     
-    row['red_avg_games_played_season'], row['red_avg_winrate_season'] = get_avg_games_season(red_alliance)
-    row['blue_avg_games_played_season'], row['blue_avg_winrate_season'] = get_avg_games_season(blue_alliance)
+    row['red_avg_games_played_season'], row['red_avg_winrate_season'] = get_avg_games(red_alliance, boundary='_season')
+    row['blue_avg_games_played_season'], row['blue_avg_winrate_season'] = get_avg_games(blue_alliance, boundary='_season')
+
+    row['red_points_ratio_season'] = get_red_points_ratio(red_alliance, blue_alliance)
     
     row['red_avg_age'] = get_avg_age(red_alliance, row['time'].year)
     row['blue_avg_age'] = get_avg_age(blue_alliance, row['time'].year)
     
-    row['red_points_ratio'] = get_red_points_ratio(red_alliance, blue_alliance)
+    row['red_elo'] = get_elo_average(red_alliance)
+    row['blue_elo'] = get_elo_average(blue_alliance)
+    
+    red_pred = get_elo_prediction(row['red_elo'], row['blue_elo'])
+    blue_pred = get_elo_prediction(row['blue_elo'], row['red_elo'])
+    
+    row['elo_prediction'] = red_pred
     
     if row['red_won'] == 1:
         for team_key in red_alliance:
-            team_data[team_key]['games_won'] += 1
-            team_data[team_key]['games_won_season'] += 1
-    else:
-        for team_key in blue_alliance:
-            team_data[team_key]['games_won'] += 1
             team_data[team_key]['games_won_season'] += 1
             
+        change_elo(red_alliance, red_pred, 1)
+        change_elo(blue_alliance, blue_pred, 0)
+    else:
+        for team_key in blue_alliance:
+            team_data[team_key]['games_won_season'] += 1
+            
+        change_elo(red_alliance, red_pred, 0)
+        change_elo(blue_alliance, blue_pred, 1)
+            
     for team_key in (blue_alliance + red_alliance):
-        team_data[team_key]['games_played'] += 1
         team_data[team_key]['games_played_season'] += 1
        
     for team_key in red_alliance:
         team_data[team_key]['points_season'] += row['red_score']
         
     for team_key in blue_alliance:
-        team_data[team_key]['points_season'] += row['blue_score']
+        team_data[team_key]['points_season'] += row['red_score']
+        
+    #Add new event awards if previous event has passed
+    num_of_matches_per_event[row['event_key']] -= 1
+    if num_of_matches_per_event[row['event_key']] <= 0:
+        add_event_awards(row['event_key'])
+        # reset_match(row['event_key'])
     
     match_df.loc[index] = row
     
@@ -282,8 +319,6 @@ match_df['avg_games_played_diff'] = match_df['red_avg_games_played'] - match_df[
 match_df['avg_age_diff'] = match_df['red_avg_age'] - match_df['blue_avg_age']
 match_df['avg_winrate_diff_season'] = match_df['red_avg_winrate_season'] - match_df['blue_avg_winrate_season']
 match_df['avg_games_played_diff_season'] = match_df['red_avg_games_played_season'] - match_df['blue_avg_games_played_season']
-match_df['red_avg_points_higher'] = 0
-match_df.loc[match_df[match_df['red_points_ratio'] > 1].index, 'red_avg_points_higher'] = 1
 
 #%%
 #Get the use case dataframe and create a csv
